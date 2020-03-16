a59484898db92cdca7f7563f595c5069a27a1907
Adam <adamos2375@gmail.com>
2020-03-16 18:37:26 +0100

    better way to overwrite class

--- sh_itcommand.lua
+++ sh_itcommand.lua
@@ -2,7 +2,7 @@ PLUGIN.name = "Edycja czatu"
 PLUGIN.desc = "Kolor dla /it, dodaje odpowiednik tej komendy na /do także"
 PLUGIN.license =  "MIT not for use on Kaktusownia opensource.org/licenses/MIT"
 
-do
+function PLUGIN:InitializedChatClasses()
 	timer.Simple(.1, function()
 		ix.chat.Register("it", {
 			OnChatAdd = function(self, speaker, text)
