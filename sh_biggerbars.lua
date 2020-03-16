8b0b6b4abe7fd98366a0491b8691f91b72477124
Adam <adamos2375@gmail.com>
2020-03-16 18:12:03 +0100

    dwa pluginy nowe

---
+++ sh_biggerbars.lua
@@ -0,0 +1,8 @@
+PLUGIN.name = "Bigger Bars"
+PLUGIN.desc = "Now bars are bigger"
+PLUGIN.author = "Lechu2375"
+PLUGIN.license =  "MIT not for use on Kaktusownia opensource.org/licenses/MIT"
+
+if CLIENT then 
+    BAR_HEIGHT = 11.5
+end
---
+++ sh_itcommand.lua
@@ -0,0 +1,19 @@
+PLUGIN.name = "Edycja czatu"
+PLUGIN.desc = "Kolor dla /it, dodaje odpowiednik tej komendy na /do także"
+PLUGIN.license =  "MIT not for use on Kaktusownia opensource.org/licenses/MIT"
+
+do
+	timer.Simple(.1, function()
+		ix.chat.Register("it", {
+			OnChatAdd = function(self, speaker, text)
+						
+				chat.AddText(Color(142, 68, 173), "(("..speaker:GetName().."))".."** "..text)
+			end,
+			CanHear = ix.config.Get("chatRange", 280) * 2,
+			prefix = {"/It","/Do"},
+			description = "@cmdIt",
+			indicator = "chatPerforming",
+			deadCanChat = true
+		})
+	end)
+end
